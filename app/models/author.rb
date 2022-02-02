f38a05d556fcd6fcd6c58adb139ae361daed2a46
Hannah McCullough <hmac091@gmail.com>
2022-02-02 11:31:00 -0700

    commit tree 99b4eca665574808609e3e614032dbf6b3dd1463

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+
+    # all authors have a name & no two authors have the same name
+    validates :name, presence: true, uniqueness: true
+    # phone numbers are exactly ten digits
+    validates :phone_number, length: { is: 10 }
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+
+    # all posts have a title
+    validates :title, presence: true
+    # content is at least 250 chars
+    validates :content, length: { minimum: 250 }
+    # summary is a maximum of 250 chars
+    validates :summary, length: { maximum: 250 }
+    # category is either Fiction or Non-Fiction
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # custom validator --> title is clickbait-y -> must contain provided list of words... 
+    validate :title_is_clickbaity
+
+    def title_is_clickbaity
+        if title && unless title.include?("Won't Believe" || "Secret" || "Top %[number]" || "Guess")
+                errors.add(:title, "Your title isn't clickbait-y!")
+            end
+        end
+    end
+
 end
